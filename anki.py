9ffb87edbd303076f20f2392a708215427c16033
Anatolii Stepaniuk <astepaniuk@tektelic.com>
2021-04-27 09:17:12 +0300

    Updated previous cards to embed google docs pages

--- anki.py
+++ anki.py
@@ -25,5 +25,56 @@ def add_note(front, back):
     requests.post('http://localhost:8765', json.dumps(payload))
 
 
+def update_note(note_id, front, back):
+    payload = {
+        "action": "updateNoteFields",
+        "version": 6,
+        "params": {
+            "note": {
+                "id": 1,
+                "fields": {
+                    "Front": "front content",
+                    "Back": "back content"
+                }
+            }
+        }
+    }
+
+    payload['params']['note']['id'] = note_id
+    payload['params']['note']['fields']['Front'] = front
+    payload['params']['note']['fields']['Back'] = back
+
+    json.dumps(payload)
+
+    requests.post('http://localhost:8765', json.dumps(payload))
+
+
+def find_notes():
+    payload = {
+        "action": "findNotes",
+        "version": 6,
+        "params": {
+            "query": "deck:Programming"
+        }
+    }
+    json.dumps(payload)
+
+    response = requests.post('http://localhost:8765', json.dumps(payload))
+    return json.loads(response.content)['result']
+
+
+def notes_info(note_id):
+    payload = {
+        "action": "notesInfo",
+        "version": 6,
+        "params": {
+            "notes": [note_id]
+        }
+    }
+    json.dumps(payload)
+
+    response = requests.post('http://localhost:8765', json.dumps(payload))
+    return json.loads(response.content)['result']
+
 def html_link(url):
     return f'<a href="{url}">LINK</a>'
---
+++ update_anki.py
@@ -0,0 +1,26 @@
+from anki import update_note
+from anki import find_notes
+from anki import notes_info
+
+
+def main():
+    for id in find_notes():
+        note = notes_info(id)[0]
+        front = note['fields']['Front']['value']
+        back = note['fields']['Back']['value']
+        update_note(id, front, new_value(back))
+
+
+def new_value(value):
+    return value if value.find("<a href=") == -1 else transform_value(value)
+
+
+def transform_value(value):
+    first = value.find('<a href="')+len('<a href="')
+    last = value.find('"', first)
+    url = value[first:last]
+    return f'<embed height="800px" src="{url}" type="text/html" width="100%">'
+
+
+if __name__ == '__main__':
+    main()
